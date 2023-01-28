859f9882e49ab3d37ba9f3c993c76fb82207e2ed
Ivan Dovbnya <113626180+SupreLTD@users.noreply.github.com>
2023-01-28 19:26:48 +0300

    Update settings.py

--- cafe_trainee/settings.py
+++ cafe_trainee/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+^ooot+w!x2yd(6305_z47y+4o1yx77!kxm=jv*9u$w42(x5^*'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
